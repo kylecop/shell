92daf7f67e509f731aa7a5e7231ab0d0a0588077
kylecoppedge <kylecoppedge@KYLECOPPEDG4702>
2020-02-23 20:50:50 -0600

    commit

--- shell/Form1.Designer.cs
+++ shell/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.username = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.shell = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.username,
+            this.shell});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(111, 94);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1015, 669);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // username
+            // 
+            this.username.Text = "Username";
+            this.username.Width = 300;
+            // 
+            // shell
+            // 
+            this.shell.Text = "shell";
+            this.shell.Width = 200;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1161, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enable";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1161, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 65);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Disable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1414, 886);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader username;
+        private System.Windows.Forms.ColumnHeader shell;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- shell/Form1.cs
+++ shell/Form1.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Principal;
+using System.Management;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Security.AccessControl;
 
 namespace shell
 {
@@ -16,5 +22,119 @@ namespace shell
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            GetLocalUserAccounts();
+        }
+        
+        private void GetLocalUserAccounts()
+        {
+
+            ManagementObjectSearcher usersSearcher = new ManagementObjectSearcher(@"SELECT * FROM Win32_UserAccount");
+            ManagementObjectCollection users = usersSearcher.Get();
+
+            var localUsers = users.Cast<ManagementObject>().Where(
+                u => (bool)u["LocalAccount"] == true &&
+                     //(bool)u["Disabled"] == false &&
+                     //(bool)u["Lockout"] == false &&
+                     //int.Parse(u["SIDType"].ToString()) == 1 &&
+                     u["Name"].ToString() != "HomeGroupUser$");
+
+            
+            foreach (ManagementObject user in users)
+            {
+                string username = user["Name"].ToString();
+                string path = @"C:\Users\" + username + @"\NTUSER.DAT";
+                string shellLocation = "";
+                listView1.Items.Add(new ListViewItem(new[] { username, getReg(username)}));
+
+            }//HKEY_USERS\S-1-5-21-798898283-2169997705-2806997698-1000\Software\Microsoft\Windows NT\CurrentVersion\Winlogon
+        }
+        private string getReg(string username)
+        {
+            string result = "";
+            int flag = 0;
+
+            string user_path = @"C:\Users\";
+            string subkey_exists = "Shell";
+
+            //if (Directory.Exists(user_path))
+            {
+                //string[] sub_dirs = Directory.GetDirectories(user_path);
+
+                //foreach (string dir in sub_dirs)
+                {
+
+
+                    string wimHivePath = @"c:\users\" + username + @"\ntuser.dat";
+                    //MessageBox.Show(wimHivePath);
+                    string loadedHiveKey = RegistryInterop.Load(wimHivePath);
+
+                    RegistryKey rk = Registry.Users.OpenSubKey(loadedHiveKey);
+
+                    if (rk != null)
+                    {
+                        RegistryKey ExistKey = rk.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\Winlogon", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                        
+                        if (ExistKey != null)
+                            if(ExistKey.GetValue("shell").ToString() != null)
+                                result = ExistKey.GetValue("shell").ToString();
+                            
+                        ExistKey.Close();
+                        
+
+                        rk.Close();
+                    }
+
+                    RegistryInterop.Unload();
+
+                }
+            }
+            
+            return result;
+        }
+        private void SetReg(string username, string value)
+        {
+            string loadedHiveKey = RegistryInterop.Load(@"c:\users\" + username + @"\ntuser.dat");
+            RegistryKey rk = Registry.Users.OpenSubKey(loadedHiveKey, true);
+            if (rk != null)
+            {
+                RegistryKey ExistKey = rk.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\Winlogon", true);
+                ExistKey.SetValue(@"Shell", value, RegistryValueKind.String);
+                ExistKey.Close();
+                rk.Close();
+            }
+            RegistryInterop.Unload();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+                button1.Enabled = true;
+            else
+                button1.Enabled = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //MessageBox.Show(listView1.SelectedItems[0].Text);
+            SetReg(listView1.SelectedItems[0].Text, @"c:\test.exe");
+            listView1.Items.Clear();
+            GetLocalUserAccounts();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //MessageBox.Show(listView1.SelectedItems[0].Text);
+            SetReg(listView1.SelectedItems[0].Text, "explorer.exe");
+            listView1.Items.Clear();
+            GetLocalUserAccounts();
+        }
     }
+
 }
